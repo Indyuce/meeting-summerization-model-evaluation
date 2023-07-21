90140eac511c5f445eec5b3e5eb4a62dd56e57e8
Jules <jpeyrat@linagora.com>
2023-07-21 15:19:03 +0200

    Found examples for AMI

--- python/xgen_few_shot_learning/find_examples.py
+++ python/xgen_few_shot_learning/find_examples.py
@@ -7,6 +7,8 @@ from datasets import load_dataset
 from transformers import AutoTokenizer
 from huggingface_login import perform_login
 
+MAXIMUM_LENGTH = 4096
+
 def mkdir(folder_path):
     try:
         os.mkdir(folder_path)
@@ -21,64 +23,32 @@ dataset = load_dataset("TalTechNLP/AMIsum", split='train')
 model_name = 'legendhasit/xgen-7b-8k-inst-8bit'
 tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
 
-csv_file = open('sequence_token_length.csv', 'w', encoding = 'utf-8')
+def format_transcript(transcript):
+    return transcript.replace(' <', '\n<')
 
+# Find entries and sort
+entries = []
 for i in range(len(dataset)):
     
     # Sample info
     sample = dataset[i]
     summary = sample['summary']
-    transcript = sample['transcript']
+    transcript = format_transcript(sample['transcript'])
     id = sample['id']
 
-    tokenized_length = len(tokenizer(dialogue, return_tensors="pt").to('cuda')['input_ids'][0])
+    tokenized_length = len(tokenizer(transcript, return_tensors="pt").to('cuda')['input_ids'][0])
+    if tokenized_length <= MAXIMUM_LENGTH:
+        entries.append((id, tokenized_length, summary, transcript))
+entries = sorted(entries, key=(lambda entry: entry[1]))
 
-    # Write
-    csv_file.write(id + ' -> ' + str(tokenized_length))
+# Write entries to folder
+print('Writing to file..')
+mkdir('examples')
+for entry in entries:
+    target_file = open('examples/' + str(entry[1]) + '_' + entry[0] + '.txt', 'w', encoding = 'utf-8')
+    target_file.write(entry[3])
+    target_file.write('\n----------------------------------------------------------------------------------------\n')
+    target_file.write(entry[2])
+    target_file.close()
 
-csv_file.close()
 print('Done!')
-
-"""
-
-print(dataset.features)
-print('Found total of', len(dataset), 'samples')
-indices = list(range(len(dataset)))
-random.shuffle(indices)
-
-
-def format_transcript(transcript):
-    return transcript.replace(' <', '\n<')
-
-counter = 0
-skipped = 0
-for random_index in indices:
-    if counter >= MAX_SAMPLES:
-        break
-
-    # Get info from dataset
-    dataset_sample = dataset[random_index]
-    reference = dataset_sample['summary']
-    id = dataset_sample['id']
-    dialogue = format_transcript(dataset_sample['transcript'])
-
-    # Check dialogue length
-    if tokenized_length > MAX_SEQUENCE_LENGTH:
-        print('-- Skipped high length sample ' + id + ':', tokenized_length)
-        skipped += 1
-        continue
-
-    counter += 1
-
-    # Write to file
-    target_text_file = open('input/ami/texts/sample_' + str(counter) + '.txt', 'w', encoding = 'utf-8')
-    target_text_file.write(dialogue)
-    target_text_file.close()
-    
-    target_text_file = open('input/ami/summaries/sample_' + str(counter) + '.txt', 'w', encoding = 'utf-8')
-    target_text_file.write(reference)
-    target_text_file.close()
-
-print('Found', counter, 'samples after skipping', skipped, 'samples')
-"""
-
