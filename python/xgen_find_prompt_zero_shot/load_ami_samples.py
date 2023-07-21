55fa75aea1fb719b6261e5055f4ee3eecc3320b9
Jules <jpeyrat@linagora.com>
2023-07-21 10:16:30 +0200

    ami samples used in litterature

--- python/xgen_find_prompt_zero_shot/load_ami_samples.py
+++ python/xgen_find_prompt_zero_shot/load_ami_samples.py
@@ -26,12 +26,15 @@ mkdir('input/' + DATASET_NAME + '/summaries')
 # Login to HF
 perform_login()
 
-dataset = load_dataset("knkarthick/AMI")
-train_dataset = dataset['test']
-left = list(range(len(train_dataset)))
+dataset = load_dataset("TalTechNLP/AMIsum", split='test')
+print(dataset.features)
+left = list(range(len(dataset)))
 
 model_name = 'legendhasit/xgen-7b-8k-inst-8bit'
 tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+
+def format_transcript(transcript):
+    return transcript.replace(' <', '\n<')
 
 i = 0
 while len(left) > 0 and i < N_SAMPLES:
@@ -42,9 +45,9 @@ while len(left) > 0 and i < N_SAMPLES:
     left.pop(random_index)
 
     # Get info from dataset
-    dataset_sample = train_dataset[random_index]
+    dataset_sample = dataset[random_index]
     reference = dataset_sample['summary']
-    dialogue = dataset_sample['dialogue']
+    dialogue = format_transcript(dataset_sample['transcript'])
 
     # Check dialogue length
     tokenized_length = len(tokenizer(dialogue, return_tensors="pt").to('cuda')['input_ids'][0])
@@ -55,11 +58,11 @@ while len(left) > 0 and i < N_SAMPLES:
     i += 1
 
     # Write to file
-    target_text_file = open('input/ami/texts/sample_' + str(i + 1) + '.txt', 'w', encoding = 'utf-8')
+    target_text_file = open('input/ami/texts/sample_' + str(i) + '.txt', 'w', encoding = 'utf-8')
     target_text_file.write(dialogue)
     target_text_file.close()
     
-    target_text_file = open('input/ami/summaries/sample_' + str(i + 1) + '.txt', 'w', encoding = 'utf-8')
+    target_text_file = open('input/ami/summaries/sample_' + str(i) + '.txt', 'w', encoding = 'utf-8')
     target_text_file.write(reference)
     target_text_file.close()
 
