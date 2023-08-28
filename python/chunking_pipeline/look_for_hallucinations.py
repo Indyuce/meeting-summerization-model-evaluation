be27eb2d7fe004ecdc74b4dfc5b91c864e8f60da
Jules <jpeyrat@linagora.com>
2023-08-28 10:50:02 +0200

    edit script

--- python/chunking_pipeline/look_for_hallucinations.py
+++ python/chunking_pipeline/look_for_hallucinations.py
@@ -12,11 +12,11 @@ def mkdir(path):
     except:
         pass
 
-mkdir('halluc')
+mkdir('halluc.ignored')
 
 for sample in samples:
     mkdir('halluc/' + sample)
-    shutil.copyfile('input/summaries/' + sample + '.txt', 'halluc/' + sample + '/gold.txt')
-    shutil.copyfile('input/texts/' + sample + '.txt', 'halluc/' + sample + '/transcript.txt')
-    shutil.copyfile('intermediate/xgen_7b_2sl_1kt/' + sample + '.txt', 'halluc/' + sample + '/1kt.txt')
-    shutil.copyfile('intermediate/xgen_7b_2sl_1kt/' + sample + '.txt', 'halluc/' + sample + '/5kt.txt')
+    shutil.copyfile('input/summaries/' + sample + '.txt', 'halluc.ignored/' + sample + '/gold.txt')
+    shutil.copyfile('input/texts/' + sample + '.txt', 'halluc.ignored/' + sample + '/transcript.txt')
+    shutil.copyfile('intermediate/xgen_7b_2sl_1kt/' + sample + '.txt', 'halluc.ignored/' + sample + '/1kt.txt')
+    shutil.copyfile('intermediate/xgen_7b_2sl_5kt/' + sample + '.txt', 'halluc.ignored/' + sample + '/5kt.txt')
